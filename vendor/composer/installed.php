e37a049ee8c0eb6d225c88d50693b43a7bd79b75
Oleksandr Lysyi <sanchoclo@gmail.com>
2024-11-20 16:47:27 +0200

    IT-146 Add SSE lib

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4bed99b5e084a570fe1af3bdc50108e085d2c11c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,11 +11,20 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4bed99b5e084a570fe1af3bdc50108e085d2c11c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'hhxsv5/php-sse' => array(
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => '4244e4e8b416103f585cafcf813cff81061c5752',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hhxsv5/php-sse',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
