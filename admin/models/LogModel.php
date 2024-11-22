3879bd6e284bd322c3adbd15c6dfb529c6f564cf
Oleksandr Lysyi <sanchoclo@gmail.com>
2024-11-22 18:40:59 +0200

    DBG-LV-149 Fix regexp to handle long tz names

--- admin/models/LogModel.php
+++ admin/models/LogModel.php
@@ -48,7 +48,7 @@ class DBG_LV_LogModel
 
         $file = self::dbg_lv_get_log_content($path);
 
-        $pattern = '/\[.{1,20} \w{1,3}\](.*)( on line )\d*?/sU';
+        $pattern = '/\[.{1,}\](.*)( on line )\d*?/sU';
         $count = preg_match_all($pattern, $file, $matches);
 
         if (!$count) {
