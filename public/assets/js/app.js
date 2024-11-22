77439c5840b937de501eb896737ae14893fdcaa0
Oleksandr Lysyi <sanchoclo@gmail.com>
2024-11-22 20:34:05 +0200

    DBG-LV-148  Add carret for collapsing the sidebar blocks

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,7 @@
         switch (new URL(location.href).searchParams.get('page')) {
             case 'debug-log-viewer':
                 import("./components/log-view.js");
+                import("./components/block-toggler.js");
                 break;
         }
     } catch (error) {
